33805095a7546e065ac4124ff0ff1e4a8aafcc66
Ansh <anshkapoor502@gmail.com>
2024-04-13 13:54:57 +0530

    setting up front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import {BrowserRouter, Routes ,Route} from 'react-router-dom'
+import Register from './components/Register'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
